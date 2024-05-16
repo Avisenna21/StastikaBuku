71700bb9915461304402654f8b65f4c9d3506192
Avisenna21 <91260446+Avisenna21@users.noreply.github.com>
2024-05-16 16:39:37 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace StatistikPenggunaanBuku
+{
+    public class Statistika<T>
+    {
+        public Dictionary<T, int> HitungFrekuensi(List<T> data)
+        {
+            Dictionary<T, int> frekuensi = new Dictionary<T, int>();
+
+            foreach (T item in data)
+            {
+                if (frekuensi.ContainsKey(item))
+                {
+                    frekuensi[item]++;
+                }
+                else
+                {
+                    frekuensi[item] = 1;
+                }
+            }
+
+            return frekuensi;
+        }
+    }
+}
